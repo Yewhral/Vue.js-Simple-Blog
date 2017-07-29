5eeb5422fcdbad9e9e96e534370c316a33192059
Yewhral <zabrzyjewski@gmail.com>
2017-07-29 11:02:14 +0200

    Mixins with Vue.js added

--- vuejs-blog/src/App.vue
+++ vuejs-blog/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <show-blogs></show-blogs>
-
+    <list-blogs></list-blogs>
   </div>
 </template>
 
@@ -11,11 +11,13 @@
 
 import addBlog from './components/addBlog.vue'
 import showBlogs from './components/showBlogs.vue'
+import listBlogs from './components/listBlogs.vue'
 
 export default {
   components: {
     'add-blog': addBlog,
-    'show-blogs': showBlogs
+    'show-blogs': showBlogs,
+    'list-blogs': listBlogs
   },
   data () {
     return {
--- vuejs-blog/src/components/listBlogs.vue
+++ vuejs-blog/src/components/listBlogs.vue
@@ -1,10 +1,9 @@
 <template>
   <div v-theme:column="'narrow'" id="show-blogs">
-    <h1>All Blog Articles</h1>
+    <h1>List Blog title</h1>
     <input type="text" v-model="search" placeholder="search blogs"/>
         <div v-for="blog in filteredBlogs" class="single-blog">
             <h2 v-rainbow> {{ blog.title | to-uppercase}} </h2>
-            <h3> {{ blog.body | snippet}} </h3>
         </div>
   </div>
 </template>
@@ -12,7 +11,6 @@
 
 
 <script>
-
 import searchMixin from '../mixins/searchMixins';
 
 export default {
@@ -31,7 +29,6 @@ export default {
         this.blogs = data.body.slice(0,10);
     });
    },
-
     filters:{
       toUppercase(value){
         return value.toUpperCase();
