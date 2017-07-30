ca798881f35ab03f5287f1d0fac47ac5990640a3
Yewhral <zabrzyjewski@gmail.com>
2017-07-30 17:02:38 +0200

    Posting and retrieving from Firebase

--- vuejs-blog/src/components/showBlogs.vue
+++ vuejs-blog/src/components/showBlogs.vue
@@ -4,7 +4,7 @@
     <input type="text" v-model="search" placeholder="search blogs"/>
         <div v-for="blog in filteredBlogs" class="single-blog">
             <router-link v-bind:to="'/blog/'+ blog.id"><h2 v-rainbow> {{ blog.title | to-uppercase}} </h2></router-link>
-            <h3> {{ blog.body | snippet}} </h3>
+            <h3> {{ blog.content | snippet}} </h3>
         </div>
   </div>
 </template>
@@ -26,9 +26,16 @@ export default {
 
   },
   created(){
-    this.$http.get('https://jsonplaceholder.typicode.com/posts').then(function(data){
-        console.log(data);
-        this.blogs = data.body.slice(0,10);
+    this.$http.get('https://fir-for-vue-blog.firebaseio.com/posts.json').then(function(data){
+        return data.json();
+    }).then(function(data){
+        var blogsArray = [];
+        for (let key in data){
+            data[key].id = key;
+            blogsArray.push(data[key]);
+        }
+        this.blogs = blogsArray;
+
     });
    },
 
--- vuejs-blog/src/components/singleBlog.vue
+++ vuejs-blog/src/components/singleBlog.vue
@@ -1,7 +1,7 @@
 <template>
 <div id="single-blog">
     <h1> {{blog.title}} </h1>
-    <article> {{blog.body}} </article>
+    <article> {{blog.content}} </article>
 </div>
 </template>
 
@@ -15,8 +15,11 @@ export default {
         }
      },
      created() {
-        this.$http.get('https://jsonplaceholder.typicode.com/posts/' + this.id).then(function(data){
-           this.blog = data.body;
+        this.$http.get('https://fir-for-vue-blog.firebaseio.com/posts/' + this.id + '.json').then(function(data){
+           return data.json();
+           }).then(function(data){
+            this.blog = data;
+
         });
      }
 
